41c074d1c0ee76f61d398868e6573241720e549b
Matthew Rae <mattyjrae123@hotmail.com>
2022-08-18 10:59:02 +1200

    Build and document subtract() function
    subtract() will be used later for subtraction operations.

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,18 @@
  * Returns the sum of n1 and n2
  * @param {number} n1
  * @param {number} n2
- * @returns {number} Sum of n1 and n2
+ * @returns {number} sum of n1 and n2
  */
 function add(n1, n2) {
   return n1 + n2;
+}
+
+/**
+ * Returns the result of n2 subtracted from n1
+ * @param {number} n1 
+ * @param {number} n2 
+ * @returns result of n2 subtracted from n1
+ */
+function subtract(n1, n2) {
+  return n1 - n2;
 }
