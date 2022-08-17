0cf23529a159dbfe7ca33cf6d843b1280fec723f
Matthew Rae <mattyjrae123@hotmail.com>
2022-08-18 11:05:58 +1200

    Add return type number in function documentation

--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,7 @@ function add(n1, n2) {
  * Returns the result of n2 subtracted from n1
  * @param {number} n1 
  * @param {number} n2 
- * @returns result of n1 - n2
+ * @returns {number} result of n1 - n2
  */
 function subtract(n1, n2) {
   return n1 - n2;
@@ -22,7 +22,7 @@ function subtract(n1, n2) {
  * Returns the product of n1 and n2
  * @param {number} n1 
  * @param {number} n2 
- * @returns result of n1 * n2
+ * @returns {number} result of n1 * n2
  */
 function multiply(n1, n2) {
   return n1 * n2;
@@ -32,7 +32,7 @@ function multiply(n1, n2) {
  * Returns result of n1 divided by n2
  * @param {number} n1 
  * @param {number} n2 
- * @returns result of n1 / n2
+ * @returns {number} result of n1 / n2
  */
 function divide(n1, n2) {
   return n1 / n2;
