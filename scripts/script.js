b64f0b139cbb79868ad0f5b5c3f6ca883257a5a1
Matthew Rae <mattyjrae123@hotmail.com>
2022-08-18 11:04:24 +1200

    Build and document divide() function
    divide() will be used later for division operations

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
  * Returns the sum of n1 and n2
  * @param {number} n1
  * @param {number} n2
- * @returns {number} sum of n1 and n2
+ * @returns {number} result of n2 + n2
  */
 function add(n1, n2) {
   return n1 + n2;
@@ -12,7 +12,7 @@ function add(n1, n2) {
  * Returns the result of n2 subtracted from n1
  * @param {number} n1 
  * @param {number} n2 
- * @returns result of n2 subtracted from n1
+ * @returns result of n1 - n2
  */
 function subtract(n1, n2) {
   return n1 - n2;
@@ -22,8 +22,18 @@ function subtract(n1, n2) {
  * Returns the product of n1 and n2
  * @param {number} n1 
  * @param {number} n2 
- * @returns product of n1 and n2
+ * @returns result of n1 * n2
  */
 function multiply(n1, n2) {
   return n1 * n2;
+}
+
+/**
+ * Returns result of n1 divided by n2
+ * @param {number} n1 
+ * @param {number} n2 
+ * @returns result of n1 / n2
+ */
+function divide(n1, n2) {
+  return n1 / n2;
 }
