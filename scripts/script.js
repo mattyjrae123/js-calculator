cc3faabd7dad7df10cac2578ee1f2914f97f48c2
Matthew Rae <mattyjrae123@hotmail.com>
2022-08-18 10:50:30 +1200

    Build and document add() function
    add() will be used later for addition operations.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-alert("hefhd");
+/**
+ * Returns the sum of n1 and n2
+ * @param {number} n1
+ * @param {number} n2
+ * @returns {number} Sum of n1 and n2
+ */
+function add(n1, n2) {
+  return n1 + n2;
+}
