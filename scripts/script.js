bb05493147e7ce65c13871fb04806404d0685f77
Matthew Rae <mattyjrae123@hotmail.com>
2022-08-18 11:18:13 +1200

    Build and document operate() function

--- scripts/script.js
+++ scripts/script.js
@@ -37,3 +37,28 @@ function multiply(n1, n2) {
 function divide(n1, n2) {
   return n1 / n2;
 }
+
+/**
+ * Returns result of applying the operator between n1 and n2
+ * @param {number} n1 
+ * @param {number} n2 
+ * @param {string} operator - must be one of +*-/
+ * @returns {number} result of applying the operator between n1 and n2
+ */
+function operate(n1, n2, operator) {
+  if (operator === "+") {
+    return add(n1, n2);
+  }
+
+  if (operator === "-") {
+    return subtract(n1, n2);
+  }
+
+  if (operator === "*") {
+    return multiply(n1, n2);
+  }
+
+  if (operator === "/") {
+    return divide(n1, n2);
+  }
+}
